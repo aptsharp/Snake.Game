41dc342fca2e740ee67af2a42f6068158984736c
Afonso -aptsharp <aff-on@hotmail.com>
2020-11-16 18:20:32 -0300

    Crianção dos eventos do controle e melhoramentos

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ snake[0] = {
     y: 8*box
 }
 
-let direction = "right";
+let direction = 'right';
 
 function criarBG(){
     context.fillStyle = "Lightgreen";
     context.fillRect(0,0,16*box,16*box);
 }
-
 function criarCobrinha(){
     for(i=0;i<snake.length;i++){
         context.fillStyle = "green";
@@ -22,7 +21,26 @@ function criarCobrinha(){
 
 }
 
+document.addEventListener('keydown', update); // para pegar os eventos quando acontece chama a função
+
+function update(event){ //impede que a cobrinha va para o lado errdado
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function inicarJogo(){
+
+    // criando valores no plano cartesiano
+
+    if(snake[0].x > 15* box && direction == 'right') snake[0].x=0;
+    if(snake[0].X<0 && direction == 'left') snake[0].X=16 *box;
+    if(snake[0].y>15*box && direction == 'donw') snake[0].y = 0;
+    if(snake[0].y<0 && direction == 'up') snake[0].y = 16*box;
+    
+
+
     criarBG();
     criarCobrinha();
 
@@ -33,10 +51,10 @@ function inicarJogo(){
 
     //coordenadas
 
-    if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
 
     snake.pop(); // para teriar o ultimo elemento
 
